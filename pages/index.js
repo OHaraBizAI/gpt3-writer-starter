bf132763c02aaee309cce64171560cae4e818223
Jordan O'Hara <jordanohara94@gmail.com>
2022-12-11 16:58:05 -0600

    Revert "Added basic LP functionality to index.js and created generate.js to handle OpenAI API calls."
    This reverts commit db16580fe9e005bc3cd136e2dd460e36f14ea2bd.

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,8 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
-import { useState } from 'react';
 
 const Home = () => {
-  const [userInput, setUserInput] = useState('');
-  const onUserChangedText = (event) => {
-    console.log(event.target.value);
-    setUserInput(event.target.value);
-  };
   return (
     <div className="root">
       <Head>
@@ -17,25 +11,10 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>Real Estate Listing Generator</h1>
+            <h1>sup, insert your headline here</h1>
           </div>
           <div className="header-subtitle">
-            <h2>Generate optimized real-estate listings in seconds</h2>
-          </div>
-        </div>
-        <div className="prompt-container">
-          <textarea
-            placeholder="start typing here"
-            className="prompt-box"
-            value={userInput}
-            onChange={onUserChangedText}
-          />
-          <div className="prompt-buttons">
-            <a className="generate-button" onClick={null}>
-              <div className="generate">
-                <p>Generate</p>
-              </div>
-            </a>
+            <h2>insert your subtitle here</h2>
           </div>
         </div>
       </div>
